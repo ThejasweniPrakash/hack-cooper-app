bf24c64acb913e44173d54bf5874d736deeff24f
Thejasweni Prakash <thejaswe@buffalo.edu>
2019-09-07 04:38:39 -0400

    resolving conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
 // update the route array with these
-
-
 const routes: Routes = [
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
-  },
-  {
-    path: '**',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  }
+ {
+   path: 'dashboard',
+   component: DashboardComponent,
+ },
+ {
+   path: 'profile',
+   component: ProfileComponent,
+ },
+ {
+   path: '**',
+   redirectTo: 'dashboard',
+   pathMatch: 'full'
+ }
 ];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,27 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { HeaderComponent } from './core/ui/header/header.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ImageCardComponent } from './core/ui/image-card/image-card.component';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProfileComponent,
-    HeaderComponent,
-    DashboardComponent,
-    ImageCardComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent,
+   ProfileComponent,
+   HeaderComponent,
+   DashboardComponent,
+   ImageCardComponent
+ ],
+ imports: [
+   BrowserModule,
+   NgbModule,
+   AppRoutingModule,
+   FormsModule
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
