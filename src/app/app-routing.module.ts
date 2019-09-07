80473cc378f14adbdf1689a9ae0fe058b7e41286
Thejasweni Prakash <thejaswe@buffalo.edu>
2019-09-07 02:54:49 -0400

    Adding profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 
+// update the route array with these
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent}
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
